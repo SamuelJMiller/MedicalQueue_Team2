a2c3f187dcb197f16d21c71a10a4c98a8d8eb60b
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2021-12-01 13:00:22 -0800

    Add files via upload

---
+++ MedicalQueue_Team2/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalQueue_Team2
+{
+   class Node
+   {
+
+      Node Next, myNode;
+;      
+      myNode = new Node();
+
+      myNode()
+      {
+         this.Next = new Node(Patient patient);
+         this.add(new Patient(string first, string last, int priority));
+         this
+
+      }
+
+
+
+   }
+}
