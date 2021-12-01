3694c267cea1a9e247fbcaa2ab746349fb95f0b5
martijor000 <82613327+martijor000@users.noreply.github.com>
2021-11-30 18:37:01 -0800

    Testing

--- MedicalQueue_Team2/Program.cs
+++ MedicalQueue_Team2/Program.cs
@@ -21,6 +21,25 @@ namespace MedicalQueue_Team2
             
         }
 
-      
+      public int Enqueue()
+      {  //returns number of patients in the queue after adding
+         
+         return 0;
+      }
+
+      public PriorityQueue Dequeue()
+      {  // returns a queue item which should be shown to the user
+         
+      }
+
+      public List ListL()
+      {  // Returns a list of users/priorities and their position in the queue
+         
+      }
+
+      public override string ToString()
+      {  // ToString: Override ToString() to generate a string with the queue in order[  Sue 1] [Dan 2] [Mary 2] [Bob 4] [Jim 5]
+         return base.ToString();
+      }
    }
 }
