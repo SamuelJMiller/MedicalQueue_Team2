c033312b9e937b6cbbe1715541e6ea1bf10f12f0
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2021-11-30 17:42:45 -0800

    scaffolding -AP

---
+++ MedicalQueue_Team2/Patient.cs
@@ -0,0 +1,52 @@
+﻿/* -- 	ERQueue - Patient Class
+
+Assignment: Patients are added to the queue with two pieces of information:
+	Name
+	Priority (1:highest priority – 5:lowest priority)
+	To manage this information, you will create a simple Patient class with a constructor for the above
+	and read-only properties to expose basic data (first, last, priority) data.
+
+
+Notes:
+	Andrea:  Since it is a read-only class, do you think we need the setters?
+
+*/
+
+using System;
+
+public class Patient
+{
+	public int ID;
+
+	private string first;
+	private string last;
+	private int priority;
+
+
+	public Patient()
+	{
+		this.first = "";
+		this.last = "";
+		this.priority = 0;
+	}
+
+	public Patient(string first, string last, int priority)
+   {
+		this.first = first;
+		this.last = last;
+		this.priority = priority;
+   }
+
+   getFirst() { return(this.first) }
+
+   setFirst(string first) { this.first = first; }
+
+   getLast() {	return this.last;	}
+
+	setLast(string last) { this.last = last; }
+
+   setPriority(int priority) { this.priority = priority; }
+
+	getPriority() { return this.priority}
+
+}
--- MedicalQueue_Team2/Program.cs
+++ MedicalQueue_Team2/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* --
+ * Assignment:
+ *     Main will contain a menu to allow for the queuing, dequeuing and listing the queue by accessing methods of the ERQueue.
+ *    Specifically, there should be at least three menu behaviors:
+ *    Enqueue: returns number of patients in the queue after adding
+ *    Dequeue: returns a queue item which should be shown to the user
+ *    ListL Returns a list of users/priorities and their position in the queue
+ *    ToString: Override ToString() to generate a string with the queue in order[  Sue 1] [Dan 2] [Mary 2] [Bob 4] [Jim 5]
+
+*/
+
+
+using System;
 
 namespace MedicalQueue_Team2
 {
@@ -8,5 +20,26 @@ namespace MedicalQueue_Team2
         {
             
         }
-    }
+
+      public int Enqueue()
+      {  //returns number of patients in the queue after adding
+         
+         return 0;
+      }
+
+      public PriorityQueue Dequeue()
+      {  // returns a queue item which should be shown to the user
+         
+      }
+
+      public List ListL()
+      {  // Returns a list of users/priorities and their position in the queue
+         
+      }
+
+      public override string ToString()
+      {  // ToString: Override ToString() to generate a string with the queue in order[  Sue 1] [Dan 2] [Mary 2] [Bob 4] [Jim 5]
+         return base.ToString();
+      }
+   }
 }
